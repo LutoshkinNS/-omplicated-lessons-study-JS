70d2b2496a4b72e2a59a1ceb446c2096b04e0dfc
Nikita Lutoshkin <n28280202@gmail.com>
2021-02-02 23:09:38 +0300

    bugs fixed

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 'use strict';
 
-let string = '  12345678912345678912345678911Qqweqwre ';
+let string = 'проверкапроверкапроверкапроверка  ';
 
 function typeCecking(arg) {
 	if (typeof(arg) !== 'string') {
 		console.log('Alarm! В качестве аргумента передана не строка!');
-	} else if (arg.startsWith(' ') || string.endsWith(' ')) {
-		console.log(arg.length);/*Для проверки*/
+	} else {
 		arg = arg.trim();
-		console.log(arg.length);/*Для проверки*/
-		console.log(arg);/*Для проверки*/
 	};
-	if (arg.length >= 30){
+	if (arg.length >= 30) {
 			arg = arg.slice(0, 30) + '...';
-			console.log(arg);/*Для проверки*/
-			console.log(arg.length);/*Для проверки*/
 	};
+	return arg;
 };
 
-typeCecking(string);
+console.log(typeCecking(string));
