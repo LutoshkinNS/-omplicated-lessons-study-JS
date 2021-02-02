ddffe935bdef7388b471a8ea71fa901e7b167f23
Nikita Lutoshkin <n28280202@gmail.com>
2021-02-02 20:48:09 +0300

    Lesson4 completed

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+let string = '  12345678912345678912345678911Qqweqwre ';
+
+function typeCecking(arg) {
+	if (typeof(arg) !== 'string') {
+		console.log('Alarm! В качестве аргумента передана не строка!');
+	} else if (arg.startsWith(' ') || string.endsWith(' ')) {
+		console.log(arg.length);/*Для проверки*/
+		arg = arg.trim();
+		console.log(arg.length);/*Для проверки*/
+		console.log(arg);/*Для проверки*/
+	};
+	if (arg.length >= 30){
+			arg = arg.slice(0, 30) + '...';
+			console.log(arg);/*Для проверки*/
+			console.log(arg.length);/*Для проверки*/
+	};
+};
+
+typeCecking(string);
