9fcbb46b4fd50c8456b5ad62b197695e59b2b1ee
Nikita Lutoshkin <n28280202@gmail.com>
2021-02-06 12:59:12 +0300

    Lesson 7 hard ready for testing

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+
+let week = [
+	'Sunday',
+	'Monday',
+	'Tuesday',
+	'Wednesday',
+	'Thursday',
+	'Friday',
+	'Saturday',
+];
+
+
+for (let item of week) {
+	let p = document.createElement('p');
+		p.innerHTML = item;
+		document.body.append(p);
+};
+
+
+let elements = document.querySelectorAll('p');
+
+for (let i = 0; i < elements.length; i++) {
+	
+	if (i === 0 || i === elements.length - 1) {
+		elements[i].innerHTML = "<i>" + elements[i].innerHTML + "</i>";
+		console.log(elements[i]);
+	};
+};
+
+let date = new Date();
+elements[date.getDay()].innerHTML = "<b>" + elements[date.getDay()].innerHTML + "</b>";
+
