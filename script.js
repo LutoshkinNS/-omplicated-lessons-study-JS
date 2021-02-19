e6c720fabd8276feb8f265e362183fb183931ffb
Nikita Lutoshkin <n28280202@gmail.com>
2021-02-19 16:13:39 +0300

    Lesson 13 hard ready to check

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const change = document.querySelector('#change'),
+	colorName = document.querySelector('#color'),
+	wrapper = document.querySelector('.wrapper');
+
+	wrapper.style.padding = 100 + 'px';
+	wrapper.style.height = 500 + 'px';
+	wrapper.style.textAlign = 'center';
+	change.style.width = 150 + 'px';
+	change.style.height = 50 + 'px';
+
+	
+change.addEventListener('click', () => {
+	const color = '#' + (Math.random().toString(16) + '000000').substring(2,8).toUpperCase();
+	colorName.innerHTML = color;
+	wrapper.style.background = color;
+});
