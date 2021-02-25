118ee9620cbf71147dd9ff2525a48a6e5646becc
Nikita Lutoshkin <n28280202@gmail.com>
2021-02-25 21:56:51 +0300

    Lesson18 part_1 ready

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-'use strict';
+function debounce(f, t) {
+	return function() {
+		const previousCall = this.lastCall;
+		this.lastCall = Date.now();
+		if (previousCall && ((this.lastCall - previousCall) <= t)) {
+			clearTimeout(this.lastCallTimer);
+		}
+		this.lastCallTimer = setTimeout(f, t);
+	};
+}
+
+const input = document.querySelector('.input'),
+	content = document.querySelector('.content');
+
+const write = () => {
+	content.textContent = '>' + input.value;
+};
+
+input.addEventListener('input', debounce(write, 300));
+
