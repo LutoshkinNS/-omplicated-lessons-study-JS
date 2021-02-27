4f4548603a20d5abd3011296a061fc1006485696
Nikita Lutoshkin <n28280202@gmail.com>
2021-02-27 20:12:25 +0300

    ready to check

--- Lesson18_part_1/script.js
+++ Lesson18_part_1/script.js
@@ -1,40 +1,20 @@
-function animate({ timing, draw, duration }) {
-
-	const start = performance.now();
-
-	requestAnimationFrame(function animate(time) {
-		// timeFraction изменяется от 0 до 1
-		let timeFraction = (time - start) / duration;
-		if (timeFraction > 1) timeFraction = 1;
-
-		// вычисление текущего состояния анимации
-		const progress = timing(timeFraction);
-
-		draw(progress); // отрисовать её
-
-		if (timeFraction < 1) {
-			requestAnimationFrame(animate);
+function debounce(f, t) {
+	return function() {
+		const previousCall = this.lastCall;
+		this.lastCall = Date.now();
+		if (previousCall && ((this.lastCall - previousCall) <= t)) {
+			clearTimeout(this.lastCallTimer);
 		}
-
-	});
+		this.lastCallTimer = setTimeout(f, t);
+	};
 }
 
 const input = document.querySelector('.input'),
-content = document.querySelector('.content');
+	content = document.querySelector('.content');
 
 const write = () => {
 	content.textContent = '>' + input.value;
 };
-
-animate({
-	duration: 1000,
-	timing(timeFraction) {
-		return 1 - timeFraction;
-	},
-	draw(progress) {
-		content.style.width = progress;
-	}
-});
 
 input.addEventListener('input', debounce(write, 300));
 
